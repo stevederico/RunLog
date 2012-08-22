371857152581c623aeb5d45c36b9597b4bdba275
Steve Derico <steve@stevederico.com>
2012-08-22 03:40:53 -0700

    Added Sidebar to Dashboard

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  		redirect_to root_url, :notice => "Signed Up!"
+  		redirect_to "sessions#create", :notice => "Signed Up!"
   	else
   		render "new"
   	end
