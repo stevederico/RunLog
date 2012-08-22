c2003e6075db3f70dc1d71da2f14e145de5b977e
Steve Derico <steve@stevederico.com>
2012-08-22 01:38:51 -0700

    Forward to Runs after login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
     user = User.authenticate(params[:email], params[:password])
     if user
       session[:user_id] = user.id
-      redirect_to root_url, :notice => "Logged in!"
+      redirect_to runs_path, :notice => "Logged in!"
     else
       flash.now.alert = "Invalid email or password"
       render "new"
