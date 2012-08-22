bb4fb81c9e91ce0833659c052aaafec6c3febf81
Steve Derico <steve@stevederico.com>
2012-08-22 13:19:22 -0700

    Added Login After Sign Up

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'pg'
 gem 'bcrypt-ruby', :require => "bcrypt"
 # Gems used only for assets and not required
 # in production environments by default.
-gem 'twitter-bootstrap-rails', :git => "git://github.com/seyhunak/twitter-bootstrap-rails.git", :branch => "static"
+# gem 'twitter-bootstrap-rails', :git => "git://github.com/seyhunak/twitter-bootstrap-rails.git", :branch => "static"
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,23 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(params[:user])
   	if @user.save
-  		redirect_to "sessions#create", :notice => "Signed Up!"
+  	  session[:user_id] = @user.id
+      redirect_to runs_path, :notice => "Logged in!"
+      # redirect_to root_url, :notice => "Signed up!"
+      # cookies[:auth_token] = @user.auth_token
   	else
   		render "new"
   	end
-
+  end
+  
+  def login
+    @user = User.authenticate(params[:email], params[:password])
+     if user
+       session[:user_id] = user.id
+       redirect_to runs_path, :notice => "Logged in!"
+     else
+       flash.now.alert = "Invalid email or password"
+       render "new"
+     end
   end
 end
